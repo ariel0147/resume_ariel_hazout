abe2dbe1df765af8460b308f7295167aff07214a
ariel0147 <arial055665@gmail.com>
2026-01-30 02:45:44 +0200

    הוספת קישור שניוינ×יו×נוי דף הפקויררויקט×ים

--- src/Website-content/Projects/Projects.js
+++ src/Website-content/Projects/Projects.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import styles from "./Projects.module.css";
+import { FaHtml5, FaCss3, FaJs, FaNodeJs } from 'react-icons/fa';
+import { SiArduino, SiCplusplus } from 'react-icons/si';
 
 const projectsData = [
     {
         id: 1,
-        title: "Journey worlds 3",
-        description: "משחק בסיגנון מריו וטירריה הרעיון המשחק למצוא ארבע מפתחות ולהגיע לתיבת האוצר ",
-        link: "https://github.com/ariel0147/game_666"
+        title: "Journey Worlds 3",
+        description: "משחק פלטפורמה הרפתקני בסגנון רטרו המשלב השראות ממריו וטרריה. השחקן נדרש לחקור עולמות, לאסוף ארבעה מפתחות עתיקים ולפתוח את תיבת האוצר האגדית.",
+        link: "https://github.com/ariel0147/game_666",
+        techIcons: [<FaHtml5 />, <FaCss3 />, <FaJs />]
     },
     {
         id: 2,
         title: "Simon",
-        description: "משחק סימון בבניה עם ארדיאנו",
-        link: "https://github.com/ariel0147/samin"
-    }
-    ,
+        description: "גרסה פיזית ואינטראקטיבית למשחק הזיכרון הקלאסי 'סיימון', המבוססת על בקר Arduino. הפרויקט משלב תכנות חומרה (Embedded) עם בניית מעגלים אלקטרוניים.",
+        link: "https://github.com/ariel0147/samin",
+        techIcons: [<SiCplusplus />, <SiArduino />]
+    },
     {
         id: 3,
-        title: "memory game",
-        description: "משחק זיכרון סיגנון חיות",
+        title: "Memory Game",
+        description: "משחק זיכרון קלאסי וצבעוני בנושא בעלי חיים. פותח בדגש על לוגיקת משחק, עיצוב רספונסיבי וחוויית משתמש מהנה לכל הגילאים.",
         link: "https://github.com/ariel0147/memory_game",
-        liveLink: "https://ariel0147.github.io/memory_game/"
+        liveLink: "https://ariel0147.github.io/memory_game/",
+        techIcons: [<FaJs />, <FaHtml5 />, <FaCss3 />]
     },
     {
         id: 4,
         title: "Todo List",
-        description: "אפליקציה לניהול משימות המאפשרת הוספה, מחיקה, סינון וניהול מעקב אחר מטלות יומיומיות.",
-        link: "https://github.com/ariel0147/todo_list_2"
+
+        description: "מערכת חכמה לניהול משימות יומיומיות. האפליקציה מאפשרת מעקב יעיל, סינון לפי סטטוס ביצוע, עריכה דינמית וממשק משתמש נקי ונוח.",
+        link: "https://github.com/ariel0147/todo_list_2",
+        techIcons: [<FaNodeJs />, <FaJs />, <FaHtml5 />, <FaCss3 />]
     },
     {
         id: 5,
         title: "Project Management",
-        description: "אפליקציה לניהול פרויקטים,הוספה,מחיקה,עריכה,מערך הצבעות",
-        link: "https://github.com/ariel0147/finel_projct"
-    }
 
+        description: "פלטפורמה מקיפה לניהול פרויקטים וצוותים. כוללת פיצ'רים של יצירת פרויקטים, מעקב סטטוסים, ומערכת הצבעות דמוקרטית לקבלת החלטות.",
+        link: "https://github.com/ariel0147/finel_projct",
+        techIcons: [<FaNodeJs />, <FaJs />, <FaHtml5 />, <FaCss3 />]
+    }
 ];
 
 function Projects(props) {
@@ -48,8 +55,18 @@ function Projects(props) {
                     <div key={project.id} className={styles.projectCard}>
                         <h3>{project.title}</h3>
                         <p>{project.description}</p>
+
+
+                        <div className={styles.techStack}>
+                            {project.techIcons && project.techIcons.map((icon, index) => (
+                                <span key={index} className={styles.techIcon}>
+                                    {icon}
+                                </span>
+                            ))}
+                        </div>
+
                         <div className={styles.buttonsContainer}>
-                            <a href={project.link} target="_blank" rel="noopener noreferrer">צפה בפרויקט</a>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer">צפה בקוד</a>
 
                             {project.liveLink && (
                                 <a href={project.liveLink} target="_blank" rel="noopener noreferrer">שחק עכשיו</a>
